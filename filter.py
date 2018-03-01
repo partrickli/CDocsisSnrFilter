8cdbb7807170b549771b0cd44f0dc27f54dbe7f7
李桂炎 <partrick.li@gmail.com>
2018-03-01 10:02:31 +0800

    filtered uesful lines contain interface and mer information

--- filter.py
+++ filter.py
@@ -1,9 +1,20 @@
 #!/usr/bin/env python
 
-lines = [l for l in open("./signal")]
+def snrRelated(l):
+  if "I/F" in l or "MER(dB)" in l:
+    return True
+
 
 snr = {}
 
+lines = [l for l in open("./signal")]
 lineCount = len(lines)
+
+filtered = [l.strip() for l in lines if snrRelated(l)]
+nodeStrings = [(filtered[i], filtered[i+1]) for i in range(len(filtered)/2)]
+for s in nodeStrings:
+  print("node")
+  print(s)
+
 
 
